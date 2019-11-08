f071a62991f1cb35d4fdc3cdec2d2080fe6a2702
banorton <banorton1@gmail.com>
2019-11-07 19:29:50 -0500

    concatenized all data with angle and sorted data by the angle

--- labF/main.m
+++ labF/main.m
@@ -10,6 +10,12 @@ load 2m_angle3_side1.txt
 load 2m_angle3_side2.txt
 load 2m_obj25_side1.txt
 load 2m_obj25_side2.txt
+X2mas1 = [X2m_angle1_side1,X2m_angle2_side1,X2m_angle3_side1];
+X2mas2 = [X2m_angle1_side2,X2m_angle2_side2,X2m_angle3_side2];
+X2ma1s1 = sortrows([angle, mean(X2mas1')'],1);
+X2ma1s2 = sortrows([angle, mean(X2mas2')'],1);
+X2mo25s1 = X2m_obj25_side1;
+X2mo25s2 = X2m_obj25_side2;
 
 load 4m_angle1_side1.txt
 load 4m_angle1_side2.txt
@@ -19,6 +25,14 @@ load 4m_obj25_side1.txt
 load 4m_obj25_side2.txt
 load 4m_obj10_side1.txt
 load 4m_obj10_side2.txt
+X4mas1 = [X4m_angle1_side1,X4m_angle2_side1];
+X4mas2 = [X4m_angle1_side2,X4m_angle2_side2];
+X4ma1s1 = sortrows([angle, mean(X4mas1')'],1);
+X4ma1s2 = sortrows([angle, mean(X4mas2')'],1);
+X4mo25s1 = X4m_obj25_side1;
+X4mo25s2 = X4m_obj25_side2;
+X4mo10s1 = X4m_obj10_side1;
+X4mo10s2 = X4m_obj10_side2;
 
 %% DECLARING CONSTANTS
 halfrot = .5; %millimeters
@@ -39,3 +53,9 @@ X2m_obj25_side2_dist = ( X2m_obj25_side2_start : (halfrot) : X2m_obj25_side2_sta
 
 %% PLOTTING DATA
 
+%2m CABLE, ANGLE1, SIDE1
+plot(X2ma1s1(:,1),X2ma1s1(:,2))
+%2m CABLE, ANGLE2, SIDE1
+plot(X2ma1s1(:,1),X2ma1s1(:,2))
+%2m CABLE, ANGLE2, SIDE1
+plot(X2ma1s1(:,1),X2ma1s1(:,2))
